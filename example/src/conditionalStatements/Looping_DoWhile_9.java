591605a4d6ca16e6ece542ee6f3eac937d4720c1
ShambhaviBiradar <33170276+ShambhaviBiradar@users.noreply.github.com>
2023-07-14 12:29:24 +0530

    14-07-2023 committing changes

---
+++ example/src/conditionalStatements/Looping_DoWhile_9.java
@@ -0,0 +1,12 @@
+package conditionalStatements;
+
+public class Looping_DoWhile_9 {
+	  
+		public static void main(String[] args) {  
+		    int i=1;  
+		    do{  
+		        System.out.println(i);  
+		    i++;  
+		    }while(i<=10);  
+		}  
+		}
---
+++ example/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Theory 3
+ *
+ */
+module example {
+}
